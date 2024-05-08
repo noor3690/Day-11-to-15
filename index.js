cc065325743b6b86e38b9d4bc6157db0db9b56a8
Noor-ul-Ain <164336345+noor3690@users.noreply.github.com>
2024-05-08 10:25:34 -0600

    Day 15
    100 days of coding challenge

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-// Ques # 34
-var pizzas = ['Pepproni', 'Range', 'creamy tikka'];
-pizzas.forEach(function (pizza) {
-    console.log("I like ".concat(pizza, " pizza."));
-});
-console.log('I really love pizza!');
-// Ques # 35 
-var animals = ['Dog', 'Cat', 'Parrot'];
-animals.forEach(function (animal) {
-    console.log("A ".concat(animal, " would make a great pet."));
-});
-console.log('Any of these animals would make a great pet!');
-// Ques # 36
-function shirt(size, message) {
-    console.log("Making a ".concat(size, " t-shirt with the message '").concat(message, "' printed on it."));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// qno # 41
+let magicians = ['Rubab', 'Iqra', 'Fareeha Api'];
+function showMagicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
 }
-shirt('medium', 'I love mama');
+showMagicians(magicians);
+// Ques #43
+let magician = ['Rubab', 'Iqra', 'Fareeha Api'];
+function make_great(magician) {
+    let greatmagicians = [];
+    for (let i = 0; i < magicians.length; i++) {
+        greatmagicians.push(`${magician} the Great`);
+    }
+    ;
+    return greatmagicians;
+}
+let greatmagicians = make_great(magicians.slice());
+console.log(`Original magicians: `);
+showMagicians(magicians);
+console.log(`Great magicians : `);
+showMagicians(greatmagicians);
+// QNO #44
+function sandwich(...items) {
+    console.log(` Sandwich order : `);
+    for (let i = 0; i < items.length; i++) {
+        console.log(`${items[i]}`);
+    }
+}
+console.log(`Enjoy your sandwich Maham`);
+sandwich(' capsicum', 'tomato', 'chicken');
+sandwich(' garlic chicken', 'mayo sauce');
+//  Ques # 45
+function cars(manufacture, modelName, ...extraOption) {
+    let carinfo = {
+        manufacture,
+        modelName,
+        ...Object.assign({}, ...extraOption)
+    };
+    return carinfo;
+}
+;
+let answer = cars('Honda', 'Civic', ['color', 'Black'], { features: ['navigation', 'Power window'] });
+console.log(answer);
